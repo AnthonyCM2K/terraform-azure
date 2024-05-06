ef92e1061b537314de0dbe585f5729caa5fd3cc1
Anthony Criollo <anthonycriollo22@gmail.com>
2024-05-05 22:14:34 -0500

    Azure Terraform

--- Azure-engine/main.tf
+++ Azure-engine/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=3.37.0"
     }
   }
 }
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "group_acm_001" {
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.group_acm_001.name
+  location            = azurerm_resource_group.group_acm_001.location
   address_space       = ["10.0.0.0/16"]
 }
